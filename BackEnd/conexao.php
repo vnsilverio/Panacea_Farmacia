f36a1bb27ac61e12fd339b12010bf9fde93b0d54
vnsilverio <vn.silverio@uni9.edu.br>
2025-10-28 18:55:55 -0300

    Enhance database connection error handling and create database
    Added error message for database connection failure and created a database.

--- BackEnd/conexao.php
+++ BackEnd/conexao.php
@@ -8,8 +8,23 @@ $banco = "panacea_farmacia";
 
 // Fiz a conexão do banco de dados usando MySQLi
 $conecta = new mysqli($localhost, $user, $senha, $banco);
+
+//adicionei a msg de erro caso dê erro de conexão com o banco mais pra frente
+
 if ($conecta->connect_error) {
     die("Falha na conexão com o banco de dados: " . $conecta->connect_error);
-  //adicionei a msg de erro caso dê erro de conexão com o banco mais pra frente
+}
+  
+// criando esqueleto da tabela para a segunda tela (remedios, quantidade de saida, ect).
+
+ $sql = "CREATE DATABASE remediosPF"; //fiz o nome da tabela generica, precisa mudar depois.
+if (mysqli_query ($conecta, $sql)) {
+    echo "Database criado com sucesso!!!";
+} else {
+    echo "Erro na criação da database: " mysqli_error($conecta);
+}
+
+mysqli_close ($conecta);
+    
 ?>
 
