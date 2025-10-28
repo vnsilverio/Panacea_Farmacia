99da35755e555015d518c5fdbe04e239298c6810
vnsilverio <vn.silverio@uni9.edu.br>
2025-10-28 17:23:53 -0300

    Improve database connection error handling
    Fix connection error handling and set charset.

--- BackEnd/conexao.php
+++ BackEnd/conexao.php
@@ -5,6 +5,12 @@ $user = "root";
 $senha = "";
 $banco = "panacea_farmacia";
 
+
 // Fiz a conexão do banco de dados usando MySQLi
 $conecta = new mysqli($localhost, $user, $senha, $banco);
+if ($conecta->connect_error) {
+    die("Falha na conexão com o banco de dados: " . $conecta->connect_error);
+  
+$conecta->set_charset("utf8mb4");
 ?>
+
